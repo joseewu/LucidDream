c40a63f172e425582e1e70250daa76334177d5c1
joseewu <josee@chocolabs.com>
2018-04-09 15:51:50 +0800

    initial project

---
+++ LucidDream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LucidDream
+//
+//  Created by joseewu on 2018/4/9.
+//  Copyright © 2018年 com.nietzsche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
